fbf511b240e47f46f0a2f082ce6ac3abdab212a3
Shashwat <ss44@usf.edu>
2026-02-08 13:40:40 -0500

    made files

---
+++ cc_3.js
@@ -0,0 +1,19 @@
+let customers = [
+    { 
+        name: "rj", 
+        email: "rj@gmail.com",
+        purchase: ["apple", "banana"] 
+    },
+    {   
+        name: "aiden", 
+        email: "aiden@gmail.com",
+        purchase: ["pencil", "water"] 
+    },
+    { 
+        name: "alex", 
+        email: "alex@gmail.com",
+        purchase: ["couch", "pillow"]
+
+    },
+
+];
